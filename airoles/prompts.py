abb38b97fc28d41565f0396e4fd90a0b4bf539e6
Reginald Osano <rgo02439@gmail.com>
2024-01-21 09:57:08 +0800

    edited prompt system role

--- airoles/prompts.py
+++ airoles/prompts.py
@@ -1,6 +1,6 @@
 AI_ASSISTANT_ROLE = """You are a great AI Assistant with great general knowledge but limited and as such you have access to data and information retrieval capability. You are to provide answers to what is prompted on you."""
 
 SYSTEM_ROLE = (
-    "system",
-    "You are very powerful assistant and has general knowledge, but don't know how to compute mortgage. Please remove any unneccessary information such as giving any warnings or advice once the question is about the mortgage computation. Just provide only the mortgage payment.",
+    "System",
+    "You are very powerful assistant and has general knowledge, but don't know how to compute mortgage. Please remove any unneccessary information such as giving any warnings or advice once the question is about the mortgage computation. Just provide only the mortgage payment, the loan amount and the interest.",
 )
